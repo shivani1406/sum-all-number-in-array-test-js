409a617ad54185ec45d6e86f018c2fd195789ad6
Shivani Sharma <shivani1406sharma@gmail.com>
2021-12-28 16:42:46 +0000

    implemented recursion

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const sum = require("../sum-from-a-to-b-test-js");
+
 function sumItems(array) {
   // Sum all the numbers in the array
+ 
+  let sum = 0;
+  for (let i of array) {
+  if (!Array.isArray(i)) {
+    // console.log(sum);
+       sum = sum + i;
+    } else {
+      sum = sum + sumItems(i);
+    }
+  }
+  return sum;
+  
 }
 
 module.exports = sumItems;
